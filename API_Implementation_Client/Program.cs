1e1b6e609ee6c12cd2a66d5a2f09ec2d6aa9e303
ibrahim <ibrahimagrud7@gmail.com>
2026-09-16 11:52:44 +0300

    Get Passed Students Completed

--- API_Implementation_Client/Program.cs
+++ API_Implementation_Client/Program.cs
@@ -20,7 +20,8 @@ class Program
         public string firstName { get; set; }
         public string LastName { get;   set; }
         public int Age { get; set; }
-       
+        public double Grade { get; set; }
+
 
     }
 
@@ -35,10 +36,10 @@ class Program
 
             //Bu noktada Framework deserializer, JSON key'i ile property ismini karşılaştırıp eşleştiriyor, sırayla değil isimle çalışıyor.
             //Mesela firstname'i alıyor ve JSON'da aynı isimdeki key ile eşleştiriyor. Buna mapping denir. Sonra Age'i age ile diye devam ediyor.
-            //JSON'da her bir key mesela bu örnekte id, firstname, lastName ve age { "id": 1, "firstName": "Ahmet", "lastName": "Yılmaz", "age": 20 } bu taraftaki (client side) student sını ile eşleştiriliyor.  büyük-küçük harf'i önemli değil
+            //JSON'da her bir key mesela bu örnekte id, firstname, lastName ve age { "id": 1, "firstName": "Ahmet", "lastName": "Yılmaz", "age": 20 } bu taraftaki (client side) st udent sını ile eşleştiriliyor.  büyük-küçük harf'i önemli değil
 
             //List kullanmamızın sebebi, program mimarisine daha uygun olduğu için bunu kullandık. JSON'dan  deserialize edilelebilen başka bir DS'de kullanabilridik. Tabi tüm DS'lerde olmaz. Sadece JSON'dan deserialize'a uygun olanlar bir diğer tabirle Add() metodu bulunduran DS'lerde olabilir."
-            var students = await myHttpClientObject.GetFromJsonAsync<List<Student>>("StudetnsAndMe");
+            var students = await myHttpClientObject.GetFromJsonAsync<List<Student>>("all");
 
           
             if(students!=null)
@@ -66,11 +67,40 @@ class Program
             Console.WriteLine("**********Fetching All Students*********");
 
             // Ham JSON'u string olarak al
-            var jsonString = await myHttpClientObject.GetStringAsync("RR/StudetnsAndMe");
+            //Server side'a StudentList fonsiyonun ENdpoint URL'li https://localhost:7140/api/Students olduğu ve bunu zaten main'de verildiği için bu parantez içinde sadece all dedemiz yeterli. Anca o zaman URL/endpoint doğru olur
+            var jsonString = await myHttpClientObject.GetStringAsync("all");
 
             //Tüm Json string formatında gelecek. BU string'i istediğin şekilde pars edebilirsin vs. Sonuç olakar illa class olmak zorunda değil.
             //Server bize JSON formatında bilgileri veriyor. Onu nasıl kullanacağın client'in keyfine kalmış. Ha biz yukarda ASP.NET Corê'un bize sağlamış olduğu araçları kullanarak arka planda JSON'un otomatik olarak Students sınıfımızla mapping yapılmasını sağladık. çünkü bu yöntem bizim program algoritma tasarımımız için daha kullanışlı. Eğer JSON formatonda almak  tasarımımız için daha iyi olsa idi öyle yapardık. Tamamen bize kalmış
             Console.WriteLine(jsonString);
+
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex);
+        }
+    }
+   
+
+    static async Task GetPassedStudents()
+    {
+        try
+        {
+            Console.WriteLine("**********Fetching All passed  Students*********");
+
+
+            var students = await myHttpClientObject.GetFromJsonAsync<List<Student>>("Passed");
+
+
+            if (students != null)
+            {
+                foreach (var Student in students)
+                {
+                    Console.WriteLine($"{Student.firstName} - {Student.LastName} - ID: {Student.Id} - Age: {Student.Age} - Grade: {Student.Grade}");
+                }
+            }
+
+
 
         }
         catch (Exception ex)
@@ -90,11 +120,11 @@ class Program
         //Mesela GetAllStudents fonksiyonunda Students'i almak istediğimiz için ilgili endPoint'i verdik.
         //bu URL ve endpoint URL'i önce birleştirilir öyle server'a gider. yani üsteki fonk içinde birleştirilmil link şöyle olur https://localhost:7140/api/RR/StudetnsAndMe
         //Ayrıca URL adresi şu kurala göre bileştir: Sona ekleme şeklinde olmaz. Onun yerine Eğer BaseAddress sonunda / yoksa, base'in son parçası (segment) atılır ve yerine relative path (fonk içinde verdiğimiz string yani sonradan eklenecek olan) konur.
-        //bizim örneğimizde BaseAddress = https://localhost:7140/api/ ve Relative path RR/StudentsAndMe. BaseAddress sonu / olduğu için direk ekleme olur ve URL https://localhost:7140/api/RR/StudentsAndMe olur.
-        myHttpClientObject.BaseAddress = new Uri("https://localhost:7140/api/");
+        //bizim örneğimizde BaseAddress = https://localhost:7140/api/ ve Relative path RR/StudentsAndMe. BaseAddress sonu / olduğu için direk ekleme olur ve URL https://localhost:7140/api/RR/StudentsAndMe olur. Yani günü sonunda server'a giden URL swagger'de ki gibi olmalı. Nerede oluşturduğu pek öneli değil. istersen buradan direk tüm URL'e ver, istersen sadece Students'e kadar olan kısmı ve geri kalamın istediğin yerde yap
+        myHttpClientObject.BaseAddress = new Uri("https://localhost:7140/api/Students/");
 
 
-        await GetAllStudentNoClass();
+        await GetAllStudent();
     }
 
 }
--- API_Implementation_Client/obj/Debug/net8.0/API_Implementation_Client.AssemblyInfo.cs
+++ API_Implementation_Client/obj/Debug/net8.0/API_Implementation_Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API_Implementation_Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be868d257cc4ce7db955a1a797eb90db350d36c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("API_Implementation_Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API_Implementation_Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
